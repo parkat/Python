74c3e59bffda9935d317b4fdfab88ecf59aa75ef
Parkat <95242011+parkat@users.noreply.github.com>
2024-01-19 17:10:48 +0000

    fixed commments

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import pygame module in this program
 import pygame
 from pygame.locals import *
 import random
@@ -42,14 +41,10 @@ class invader:
             self.invY += 3
 
 
-# collision helper
 def offset(mask1, mask2):
     return int(mask2.cbX - mask1.invX), int(mask2.cbY - mask1.invY)
 
 
-# activate the pygame library .
-# initiate pygame and give permission
-# to use pygame's functionality.
 pygame.init()
 
 resetDone = False
@@ -76,34 +71,29 @@ def keypress():
     global cannonxpos
     global cbY
     global cbX
-
-    # stores keys pressed
+    global gameOver
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
-        # decrement in x co-ordinate
         if not gameOver:
             if cannonxpos > 0:
                 cannonxpos -= cannonVel
 
     if keys[pygame.K_RIGHT]:
-        # increment in x co-ordinate
         if not gameOver:
             if cannonxpos < WINDOW_WIDTH - 20:
                 cannonxpos += cannonVel
 
     elif keys[pygame.K_ESCAPE]:
         resetDone = False
+        gameOver = False
 
     if keys[pygame.K_SPACE]:
         if not gameOver:
             cannonBalls.append(cannonBall(cannonxpos, cannonypos))
-    #  print(len(cannonBalls))
 
 
 def paint():
-    # completely fill the surface object
-    # with black colour
     global score
     win.fill((0, 0, 0))
 
@@ -124,15 +114,11 @@ def paint():
         win.blit(img, (10, WINDOW_HEIGHT / 2))
 
 
-# def animate():
-
 def drawcannon(x, y, width, height):
-    # Creating rectangle using the draw.rect() method
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
 
 
 def drawCannonBall(x, y, radius):
-    # for x in range(cannonBalls[]
     pygame.draw.circle(win, (255, 255, 255), (x, y), radius)
 
 
@@ -140,29 +126,18 @@ def drawInvader(x, y):
     pygame.draw.polygon(win, (0, 255, 0), [[x, y], [x + 20, y], [x + 10, y + 20]])
 
 
-# create the display surface object
-
 win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# set the pygame window name
 pygame.display.set_caption("SpaceInvaders")
 
-# Indicates pygame is running
 run = True
 
-# infinite loop
 while run:
     pygame.time.delay(int(1000 / FRAME_RATE))
 
-    # iterate over the list of Event objects
-    # that was returned by pygame.event.get() method.
     for event in pygame.event.get():
 
-        # if event object type is QUIT
-        # then quitting the pygame
-        # and program both.
         if event.type == pygame.QUIT:
-            # it will make exit the while loop
             run = False
         else:
             mousepress(event)
@@ -171,7 +146,6 @@ while run:
         x.animate()
         if x.cbY < 20:
             cannonBalls.remove(x)
-            print(len(cannonBalls))
 
     for x in invaders.copy():
         x.animate()
@@ -179,7 +153,7 @@ while run:
             invaders.clear()
             gameOver = True
             reset()
-    # collision code
+
     for x in invaders.copy():
         for y in cannonBalls.copy():
             if x.mask.overlap(y.mask, offset(x, y)):
@@ -196,8 +170,6 @@ while run:
     keypress()
     paint()
 
-    # it refreshes the window
     pygame.display.update()
 
-# closes the pygame window
 pygame.quit()
